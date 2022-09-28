c0340de740327a5b5e0608fa95dcb639bbfadd07
FrowningCat <vsavinkov60@gmail.com>
2022-09-28 18:08:17 +0300

    fix

--- homework/fourth_task.py
+++ homework/fourth_task.py
@@ -1,5 +1,5 @@
 print('Write a program that accepts 2 numbers as input. Specify a list of N elements filled with numbers from the'
-      ' interval [-N, N]. Find the product of the elements at the specified positions (not indexes)')
+      ' interval [-N, N].Find the product of the elements at the specified positions (not indexes)')
 n = int(input('Enter number of elements '))
 x = int(input('Enter position one ')) -1
 y = int(input('Enter position two ')) -1
